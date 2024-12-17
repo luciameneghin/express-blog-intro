50cb250cc2e96a244c3692b0e802918dd5ae8b0c
luciameneghin <lucia.meneghin00@gmail.com>
2024-12-17 14:45:47 +0100

    feat: created an array of objects and imported into the main file

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const posts = require('./modules/post')
+console.log(posts);
+
+
 app.get('/', (req, res) => {
   res.send('Server del mio blog')
 })
---
+++ modules/post.js
@@ -0,0 +1,41 @@
+const posts = [
+  {
+    title: 'post 1',
+    content: 'contenuto 1',
+    image: 'url',
+    tags: ['tag1', 'tag2', 'tag3']
+  },
+  {
+    title: 'post 2',
+    content: 'contenuto 2',
+    image: 'url',
+    tags: ['tag1', 'tag2', 'tag3']
+  },
+  {
+    title: 'post 2',
+    content: 'contenuto 2',
+    image: 'url',
+    tags: ['tag1', 'tag2', 'tag3']
+  },
+  {
+    title: 'post 3',
+    content: 'contenuto 3',
+    image: 'url',
+    tags: ['tag1', 'tag2', 'tag3']
+  },
+  {
+    title: 'post 4',
+    content: 'contenuto 4',
+    image: 'url',
+    tags: ['sole', 'tag2', 'tag3']
+  },
+  {
+    title: 'post 5',
+    content: 'contenuto 5',
+    image: 'url',
+    tags: ['tag1', 'tag2', 'tag3']
+  }
+];
+
+
+module.exports = posts;
