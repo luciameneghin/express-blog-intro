45d5179d17cbac08c1c9cefbf3e0f00c44fbed8b
luciameneghin <lucia.meneghin00@gmail.com>
2024-12-17 15:01:54 +0100

    feat: created a root '/bacheca'  with res.json

--- app.js
+++ app.js
@@ -3,12 +3,22 @@ const app = express();
 const port = 3000;
 
 const posts = require('./modules/post')
-console.log(posts);
 
 
 app.get('/', (req, res) => {
   res.send('Server del mio blog')
 })
+
+
+app.get('/bacheca', (req, res) => {
+  res.json({
+    post: posts,
+    counter: posts.length,
+  })
+})
+
+
+
 
 app.listen(port, () => {
   console.log(`port: ${port}`)
--- modules/post.js
+++ modules/post.js
@@ -12,12 +12,6 @@ const posts = [
     tags: ['tag1', 'tag2', 'tag3']
   },
   {
-    title: 'post 2',
-    content: 'contenuto 2',
-    image: 'url',
-    tags: ['tag1', 'tag2', 'tag3']
-  },
-  {
     title: 'post 3',
     content: 'contenuto 3',
     image: 'url',
@@ -27,11 +21,17 @@ const posts = [
     title: 'post 4',
     content: 'contenuto 4',
     image: 'url',
-    tags: ['sole', 'tag2', 'tag3']
+    tags: ['tag1', 'tag2', 'tag3']
   },
   {
     title: 'post 5',
     content: 'contenuto 5',
+    image: 'url',
+    tags: ['sole', 'tag2', 'tag3']
+  },
+  {
+    title: 'post 6',
+    content: 'contenuto 6',
     image: 'url',
     tags: ['tag1', 'tag2', 'tag3']
   }
